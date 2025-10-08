dc3856e0a4b838fe8f70edf616b852146dbe2618
OmBRz <omarbrrcl@gmail.com>
2025-10-08 11:34:52 -0500

    feat(Profile): implement Retrofit instance and user repository methods.

---
+++ app/src/main/java/com/pinkcells/workstation/users/data/RetrofitInstance.kt
@@ -0,0 +1,30 @@
+package com.pinkcells.workstation.users.data
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://workstation-arqui-fgbngphuh0g4a8at.canadacentral-01.azurewebsites.net/api/workstation/"
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(40, TimeUnit.SECONDS)
+        .readTimeout(40, TimeUnit.SECONDS)
+        .writeTimeout(40, TimeUnit.SECONDS)
+        .addInterceptor(logging)
+        .build()
+
+    val api: UserApiService by lazy{
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserApiService::class.java)
+    }
+}
--- app/src/main/java/com/pinkcells/workstation/users/data/UsersRepository.kt
+++ app/src/main/java/com/pinkcells/workstation/users/data/UsersRepository.kt
@@ -1,4 +1,78 @@
 package com.pinkcells.workstation.users.data
 
+import android.util.Log
+import com.pinkcells.workstation.users.domain.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 object UsersRepository {
+
+    private val _usersFlow = MutableStateFlow<List<User>>(emptyList())
+    val usersFlow: StateFlow<List<User>> = _usersFlow.asStateFlow();
+
+    suspend fun fetchUserById(id: String): User? {
+        return try {
+            val response = RetrofitInstance.api.getUserById(id)
+            if (response.isSuccessful) {
+                val user = response.body()
+                user
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    suspend fun updateUser(user: User): User? {
+        Log.d("UsersRepository", "updateUser(${user.id}) - Actualizando usuario: ${user.id}")
+
+        return try {
+            if (user.id == null) {
+                Log.e("UsersRepository", "updateUser: El ID del usuario es nulo.")
+                return null
+            }
+
+            val response = RetrofitInstance.api.updateUser(user.id, user)
+            Log.d("UsersRepository", "Response code: ${response.code()}")
+
+            if (response.isSuccessful){
+                val updatedUser = response.body()
+                Log.e("UsersRepository", "Usuario actualizado: $updatedUser")
+
+                updatedUser
+            }else {
+                val errorBody = response.errorBody()?.string()
+                Log.e("UsersRepository", "Error actualizando usuario: ${response.code()} - $errorBody")
+                null
+            }
+        }catch (e: Exception){
+            Log.e("UsersRepository", "Excepción al actualizar usuario: ${e.message}", e)
+            null
+        }
+
+    }
+
+    suspend fun deleteUser(id: String): Boolean {
+        Log.d("UsersRepository", "deleteUser($id) - Eliminando usuario")
+
+        return try {
+            val response = RetrofitInstance.api.deleteUser(id)
+            Log.d("UsersRepository", "Response code: ${response.code()}")
+
+            if (response.isSuccessful){
+                Log.d("UsersRepository", "Usuario eliminado correctamente")
+                true
+            }else {
+                val errorBody = response.errorBody()?.string()
+                Log.e("UsersRepository", "Error eliminando usuario: ${response.code()} - $errorBody")
+                false
+            }
+        }catch (e: Exception){
+            Log.e("UsersRepository", "Excepción al eliminar usuario: ${e.message}", e)
+            false
+        }
+
+    }
 }
